ad7488317a4fcc717451448a7debc8731ba45689
can kurt <can.kurt.aa@gmail.com>
2020-10-12 22:37:46 +0300

    dummy return json changed for better debuging

--- deep_predictor/predictor_dummy.py
+++ deep_predictor/predictor_dummy.py
@@ -14,7 +14,7 @@ class predictor():
         if(self.is_inited):
             print("**********************INITED DUMMY PREDICTOR MAKING PREDICTION**********************")
             time.sleep(5)
-            return 200, {'predictions': {'is_confident': 1, '1': {'class_name': '1', 'confidence': 1.0, 'class_index': 1}}}, "deep_predictor\images\predictions\keras\1"
+            return 200, {"predictions": { "1": {"class_name": '1', "confidence": 0.6, "class_index": 1}, "2": {"class_name": '2', "confidence": 0.2, "class_index": 2}, "3": {"class_name": '3', "confidence": 0.1, "class_index": 3},"is_confident": 1}}, "deep_predictor/images/predictions/keras/1"
         else:
             print("**********************DUMMY PREDICTOR NOT INITED**********************")
 
@@ -23,6 +23,6 @@ class predictor():
         if(self.is_inited):
             print("**********************INITED DUMMY PREDICTOR MAKING PREDICTION**********************")
             time.sleep(5)
-            return 200, '{ "pred1" : {"class" : 1,"x1" : 1,"y1" : 1,"c1" : 1,"c2" : 1}, "pred2" : {"class" : 2,"x1" : 2,"y1" : 2,"c1" : 2,"c2" : 2}}', "deep_predictor\images\predictions\keras\1"
+            return 200, '{ "pred1" : {"class" : 1,"x1" : 1,"y1" : 1,"c1" : 1,"c2" : 1}, "pred2" : {"class" : 2,"x1" : 2,"y1" : 2,"c1" : 2,"c2" : 2}}', "deep_predictor/images/predictions/yolo/1"
         else:
             print("**********************DUMMY PREDICTOR NOT INITED**********************")
