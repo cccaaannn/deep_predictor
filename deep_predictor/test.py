fb4ba53b1f8ac1ab638e85f8751a34988e181976
can kurt <can.kurt.aa@gmail.com>
2020-10-16 00:51:06 +0300

    changes for testing

--- deep_predictor/test.py
+++ deep_predictor/test.py
@@ -1,10 +1,10 @@
-from deep_predictor import deep_predictor
+# from deep_predictor import deep_predictor
 
 
 
-dp = deep_predictor()
+# dp = deep_predictor()
 
-dp.init_predictor(darknet=False, keras=True)
+# dp.init_predictor(darknet=False, keras=True)
 # is_predicted, prediction = dp.predict_image_keras("deep_predictor/images/test_images/other/0.jpg")
 # is_predicted, prediction = dp.predict_image_keras("deep_predictor/images/test_images/other/1.jpg")
 # is_predicted, prediction = dp.predict_image_keras("deep_predictor/images/test_images/other/2.jpg")
@@ -16,11 +16,18 @@ dp.init_predictor(darknet=False, keras=True)
 # is_predicted, prediction = dp.predict_image_keras("deep_predictor/images/test_images/other/8.jpg")
 # is_predicted, prediction = dp.predict_image_keras("deep_predictor/images/test_images/other/9.jpg")
 
-is_predicted, prediction, path = dp.predict_image_keras("deep_predictor/images/test_images/food/tavuk_sis/1.jpg")
+# is_predicted, prediction, path = dp.predict_image_keras("deep_predictor/images/test_images/food/tavuk_sis/1.jpg")
 
 # dp = deep_predictor()
 #dp.init_predictor(darknet=True, keras=False)
 #is_predicted, prediction, _ = dp.predict_image_darknet("images/test_images/food/asure/2.jpg")
 
+
+
+# from database_handler import database_handler
+
+# db = database_handler()
+
+# db.delete_prediction("PV8EbJvv7mbS")
 
 
