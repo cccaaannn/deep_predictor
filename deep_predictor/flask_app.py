ee2c58f17b8feb215c1a14fa15df2e35e5748e89
can kurt <can.kurt.aa@gmail.com>
2020-10-16 00:47:11 +0300

    flask app now can create multiple predictors for prediction and tensorflow version 1 2 support is now better

--- deep_predictor/flask_app.py
+++ deep_predictor/flask_app.py
@@ -1,150 +1,190 @@
-from flask import Flask, request, render_template, redirect, abort
+# flask
+from flask import Flask, request, render_template, redirect, abort, url_for
 from werkzeug.utils import secure_filename
+
+# other imports
 import sys
 import os
 
+# my classes
+from logger_creator import logger_creator
+from database_handler import database_handler
+from prediction_thread import prediction_thread
 
-
-# cfg paths
-flask_cfg_path = "deep_predictor/cfg/flask.cfg"
-database_handler_cfg_path = "deep_predictor/cfg/database.cfg"
-deep_predctor_cfg_path = "deep_predictor/cfg/deep_predictor.cfg"
-
-
+# helpers
 from helpers.file_folder_operations import file_folder_operations
-
 from helpers.image_operations import image_operations
 
 
-cfg = file_folder_operations.read_json_file(flask_cfg_path)
+
+# creates predictor for tf version
+def create_predictors(tensorflow_version):
+    predictors = {}
+    tf_graph = None
+
+    if(tensorflow_version == "2"):
+        from deep_predictor import deep_predictor
+        for predictor in cfg["predictors"]:
+            predictors.update({
+                predictor : deep_predictor(cfg["predictors"][predictor], init=True)
+            })
+
+    elif(tensorflow_version == "1"):
+        from deep_predictor import deep_predictor
+        for predictor in cfg["predictors"]:
+            predictors.update({
+                predictor : deep_predictor(cfg["predictors"][predictor], init=True)
+            })
+        import tensorflow as tf
+        tf_graph = tf.get_default_graph()
+
+    elif(tensorflow_version == "dummy"):
+        # dummy predictor for testing without keras or tf
+        from predictor_dummy import predictor as deep_predictor
+
+        for predictor in cfg["predictors"]:
+            predictors.update({
+                predictor : deep_predictor(init=True)
+            })
+
+    else:
+        logger.critical("tensorflow version not supported")
+        raise ValueError("tensorflow version not supported")
+
+    return predictors, tf_graph
 
 
-from logger_creator import logger_creator
-logger = logger_creator().flask_logger()
 
 
 
-from database_handler import database_handler
-db = database_handler(database_handler_cfg_path)
-
-
-
-from deep_predictor import deep_predictor
-predictor = deep_predictor(deep_predctor_cfg_path)
-predictor.init_predictor(darknet = False, keras = True)
-
-# import tensorflow as tf
-# tf_graph = tf.get_default_graph()
-
-
-# dummy predictor for testing without keras or tf
-# from predictor_dummy import predictor
-# predictor = predictor()
-# predictor.init()
-
-
-
-
-from prediction_thread import prediction_thread
-
-
+# cfg path
+flask_cfg_path = "deep_predictor/cfg/flask.cfg"
 
 
 
 # create app
 app = Flask(__name__)
+
+# set config options
+cfg = file_folder_operations.read_json_file(flask_cfg_path)
+
+tensorflow_version = cfg["flask_options"]["tensorflow_version"]
+database_handler_cfg_path = cfg["flask_options"]["database_handler_cfg_path"]
+supported_extensions = cfg["flask_options"]["supported_extensions"]
+temp_save_path = cfg["flask_options"]["temp_save_path"]
+default_api_response = cfg["flask_options"]["default_api_response"]
+
 app.config["DEBUG"] = True
 app.config['MAX_CONTENT_LENGTH'] = cfg["flask_options"]["MAX_CONTENT_LENGTH"]
-app.config['UPLOAD_EXTENSIONS'] = cfg["flask_options"]["UPLOAD_EXTENSIONS"]
-app.config['UPLOAD_PATH'] = cfg["flask_options"]["UPLOAD_PATH"]
+# app.config['UPLOAD_EXTENSIONS'] = cfg["flask_options"]["UPLOAD_EXTENSIONS"]
+# app.config['UPLOAD_PATH'] = cfg["flask_options"]["UPLOAD_PATH"]
+
+
+
+# create logger and db
+logger = logger_creator().flask_logger()
+db = database_handler(database_handler_cfg_path)
+
+# create predictors
+predictors, tf_graph = create_predictors(tensorflow_version)
+
+
+
+
+
 
 
 @app.route('/', methods=['GET'])
 def home():
     logger.info("function: {0} method: {1}".format("home", request.method))
-    return render_template("index.html")
+    return render_template("index.html"), 200
 
 
 @app.route('/result', methods=['GET'])
-def result_page():
-    return redirect("upload.html")
-
-
+def result():
+    return redirect(url_for("home")), 308
 
 
 @app.route('/upload', methods=['POST', 'GET'])
-def upload_files():
+def upload_image():
     logger.info("function: {0} method: {1}".format("upload_files", request.method))
     if(request.method == 'POST'):
+        
+        # get elements form form
         uploaded_file = request.files['file']
         filename = secure_filename(uploaded_file.filename)
-
-        model_id = request.form['model_id']
+        model_name = request.form['model_name']
         prediction_id = request.form['prediction_id']
 
 
         # check form elements
-        if(filename == "" or model_id == "" or prediction_id == ""):
-            return render_template("upload.html")
+        if(filename == "" or model_name == "" or prediction_id == ""):
+            logger.warning("required form elements are empty")
+            return render_template("upload.html", models=predictors.keys()), 400
 
-        # check model id
-        if(not (model_id == "1" or model_id == "2")):
-            return render_template("upload.html")
+        # check model name
+        if(model_name not in predictors):
+            logger.warning("model name is not exists")
+            abort(400, description="model name is not exists")
 
-        # prevet collision
+        # check for collision
         if(db.is_prediction_exists(prediction_id)):
-            # db.update_prediction_status(prediction_id, 400)
-            abort(400)
+            logger.warning("prediction id exists")
+            abort(400, description="prediction id exists")
+
+        # check image extension
+        if(not image_operations.validate_extension(supported_extensions, filename)):
+            logger.warning("image extension is not supported")
+            abort(400, description="image extension is not supported")
+
+
+        unique_full_filename = file_folder_operations.create_unique_file_name(os.path.join(temp_save_path, filename))
+        uploaded_file.save(unique_full_filename)
+
+
+        # check image resizability
+        if(not image_operations.validate_image(unique_full_filename, delete = True)):
+            logger.warning("image is not supported")
+            abort(400, description="image is not supported")
+
 
         # prepare preediction
         db.create_prediction(prediction_id)
 
-
-        if(not image_operations.validate_extension(app.config['UPLOAD_EXTENSIONS'], filename)):
-            logger.warning("image extension is not supported")
-            db.update_prediction_status(prediction_id, 310)
-            # db.delete_prediction(prediction_id)
-            abort(400)
-
-
-        unique_full_filename = file_folder_operations.create_unique_file_name(os.path.join(app.config['UPLOAD_PATH'], filename))
-        uploaded_file.save(unique_full_filename)
-
-
-        if(not image_operations.validate_image(unique_full_filename, delete = True)):
-            logger.warning("image is not supported")
-            db.update_prediction_status(prediction_id, 350)
-            # db.delete_prediction(prediction_id)
-            abort(400)
-
         # start prediction on thread
-        pred_thread = prediction_thread(database_handler_cfg_path, unique_full_filename, prediction_id, int(model_id), predictor, tf_version="2", is_dummy=False)
+        pred_thread = prediction_thread(
+            database_handler_cfg_path, 
+            unique_full_filename, 
+            prediction_id, 
+            predictor=predictors[model_name], 
+            tf_version=tensorflow_version, 
+            tf_graph=tf_graph, 
+            )
+        
         pred_thread.start()
 
-        return render_template("result.html", prediction_id=prediction_id)
+        return render_template("result.html", prediction_id=prediction_id), 201
     else:
-        return render_template("upload.html")
-
+        return render_template("upload.html", models=predictors.keys()), 200
 
 
 @app.route('/api', methods=['GET'])
 def api():
     logger.info("function: {0} method: {1}".format("api", request.method))
 
+    # get args
     query_parameters = request.args
-
     prediction_id = query_parameters.get('prediction_id')
 
-    prediction = cfg["flask_options"]["dafault_api_response"]
+    prediction = default_api_response
 
     if(prediction_id):
         prediction = db.get_prediction_json(prediction_id)
 
-        return prediction
+        return prediction, 200
     else:
-        return prediction
+        return prediction, 500
     # try:
-
 
 
 
