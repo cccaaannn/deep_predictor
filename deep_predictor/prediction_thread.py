a770e739ba9c6aff127883cb8ac0043c78066e05
can kurt <can.kurt.aa@gmail.com>
2020-10-16 00:52:31 +0300

    dummy predictor and prediction thread updated for tensorflow support and new database support

--- deep_predictor/prediction_thread.py
+++ deep_predictor/prediction_thread.py
@@ -4,60 +4,46 @@ from logger_creator import logger_creator
 
 
 class prediction_thread(threading.Thread):
-    def __init__(self, database_handler_cfg_path, temp_image_path, prediction_id, model_id, predictor, tf_version="2", tf_graph = None, is_dummy=False):
+    def __init__(self, database_handler_cfg_path, temp_image_path, prediction_id, predictor, tf_version="dummy", tf_graph = None):
         super().__init__()
         self.db = database_handler(database_handler_cfg_path)
         self.logger = logger_creator().prediction_thread_logger()
         self.predictor = predictor
         self.prediction_id = prediction_id
         self.temp_image_path = temp_image_path
-        self.model_id = model_id
         self.tf_version = tf_version
         self.tf_graph = tf_graph
-        self.is_dummy = is_dummy
 
     def run(self):
         self.logger.info("prediction started on thread")
-        if(not self.is_dummy):
-            if(self.tf_version == "1"):
-                # tf_graph is needed fot model to run on a thread
-                with self.tf_graph.as_default():
-                    if(self.model_id == 1):
-                        status, prediction, image_path = self.predictor.predict_image_keras(self.temp_image_path, save_image = "save")
-                    elif(self.model_id == 2):
-                        status, prediction, image_path = self.predictor.predict_image_darknet(self.temp_image_path, save_image = "save")
-                    
-                    self.logger.info("prediction status {0}".format(status))
 
-                    if(status == 200):
-                        self.db.update_prediction(self.prediction_id, prediction, self.model_id, image_path)
-                    else:
-                        self.db.update_prediction_status(self.prediction_id, status)
+        if(self.tf_version == "1"):
+            # tf_graph is needed fot model to run on a thread
+            with self.tf_graph.as_default():
+                status, model_info, prediction, image_path = self.predictor.predict_image(self.temp_image_path, save_image = "save")
 
-            elif(self.tf_version == "2"):
-                if(self.model_id == 1):
-                    status, prediction, image_path = self.predictor.predict_image_keras(self.temp_image_path, save_image = "save")
-                elif(self.model_id == 2):
-                    status, prediction, image_path = self.predictor.predict_image_darknet(self.temp_image_path, save_image = "save")
-                
-                self.logger.info("prediction status {0}".format(status))
+        elif(self.tf_version == "2" or self.tf_version == "dummy"):
+            status, model_info, prediction, image_path = self.predictor.predict_image(self.temp_image_path, save_image = "save")
 
-                if(status == 200):
-                    self.db.update_prediction(self.prediction_id, prediction, self.model_id, image_path)
-                else:
-                    self.db.update_prediction_status(self.prediction_id, status)
+        else:
+            self.logger.error("tensorflow version not supported")
+
+        self.logger.info("prediction status {0}".format(status))
 
 
-
-        if(self.is_dummy):
-            if(self.model_id == 1):
-                status, prediction, image_path = self.predictor.predict_image_keras(self.temp_image_path, save_image = "save")
-            elif(self.model_id == 2):
-                status, prediction, image_path = self.predictor.predict_image_darknet(self.temp_image_path, save_image = "save")
-            
-            self.logger.info("prediction status {0}".format(status))
-
-            if(status == 200):
-                self.db.update_prediction(self.prediction_id, prediction, self.model_id, image_path)
-            else:
-                self.db.update_prediction_status(self.prediction_id, status)
+        # save results to db
+        if(status == 200):
+            self.db.update_successful_prediction(
+                self.prediction_id, 
+                prediction, 
+                model_info, 
+                model_info["model_id"], 
+                image_path
+                )
+        else:
+            self.db.update_failed_prediction(
+                self.prediction_id, 
+                status, 
+                model_info, 
+                model_info["model_id"]
+                )
--- deep_predictor/predictor_dummy.py
+++ deep_predictor/predictor_dummy.py
@@ -1,20 +1,29 @@
 import time
 
 class predictor():
-    def __init__(self):
+    def __init__(self, init=False):
         print("**********************DUMMY PREDICTOR STARTED**********************")
         self.is_inited = False
+
+        if(init):
+            self.init()
 
     def init(self):
         print("**********************DUMMY PREDICTOR INITED**********************")
         self.is_inited = True
 
 
-    def predict_image_keras(self, temp_image_path, save_image = ""):
+    def predict_image(self, temp_image_path, save_image = ""):
         if(self.is_inited):
             print("**********************INITED DUMMY PREDICTOR MAKING PREDICTION**********************")
-            time.sleep(5)
-            return 200, {"predictions": { "1": {"class_name": '1', "confidence": 0.6, "class_index": 1}, "2": {"class_name": '2', "confidence": 0.2, "class_index": 2}, "3": {"class_name": '3', "confidence": 0.1, "class_index": 3},"is_confident": 1}}, "deep_predictor/images/predictions/keras/1"
+            time.sleep(2)
+            return (
+                200, 
+                {'predictor_backend': 'keras', 'model_id': 200, 'image_w': 224, 'image_h': 224, 'grayscale': 0, 'return_topN': 3, 'confidence_threshold': 0.9}, 
+                {'predictions': {'is_confident': 0, '1': {'class_index': 1, 'class_name': 'adanaKenap', 'confidence': 0.72227687}, '2': {'class_index': 2, 'class_name': 'akcaabatKofte', 'confidence': 0.17435183}, '3': {'class_index': 5, 'class_name': 'bulgurPilavi', 'confidence': 0.06662769}}}, 
+                "deep_predictor/images/predictions/keras/1"
+            )
+            
         else:
             print("**********************DUMMY PREDICTOR NOT INITED**********************")
 
