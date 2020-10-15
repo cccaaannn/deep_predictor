8d3b3170fbbfd7a6a54ed9d165455c744c538ea7
can kurt <can.kurt.aa@gmail.com>
2020-10-16 00:53:12 +0300

    model info added to the database so handler functions cahnged

--- deep_predictor/database_handler.py
+++ deep_predictor/database_handler.py
@@ -18,6 +18,7 @@ class database_handler():
         
 
     def check_connection(self):
+        """checks connection to db"""
         try:
             with sqlite3.connect(self.database_path) as _:
                 self.logger.info("db connected")
@@ -26,56 +27,62 @@ class database_handler():
 
 
     def create_prediction(self, prediction_id):
+        """creates prediction with 100 status code"""
         try:
             with sqlite3.connect(self.database_path) as connection:
                 self.logger.info("function: {0} param: {1}".format("create_prediction", prediction_id))
-                query = "INSERT INTO predictions(prediction_id, prediction_status, prediction, image_path, model_id, prediction_time) VALUES(?,?,?,?,?,?);"
+                query = "INSERT INTO predictions(prediction_id, prediction_status, prediction, image_path, model_info, model_id, prediction_time) VALUES(?,?,?,?,?,?,?);"
                 cursor = connection.cursor()   
-                cursor.execute(query, (prediction_id, 100, '', '', 0, 0))
+                cursor.execute(query, (prediction_id, 100, '', '', '', 0, 0))
                 connection.commit()
         except sqlite3.IntegrityError:
             self.logger.error("", exc_info=True)
         except:
             self.logger.critical("", exc_info=True)
     
-# TODO
-    # def delete_prediction(self, prediction_id):
-    #     try:
-    #         with sqlite3.connect(self.database_path) as connection:
-    #             self.logger.info("function: {0} param: {1}".format("create_prediction", prediction_id))
-    #             query = "INSERT INTO predictions(prediction_id, prediction_status, prediction, image_path, model_id, prediction_time) VALUES(?,?,?,?,?,?);"
-    #             cursor = connection.cursor()   
-    #             cursor.execute(query, (prediction_id, 100, '', '', 0, 0))
-    #             connection.commit()
-    #     except:
-    #         self.logger.critical("", exc_info=True)
 
-
-    def update_prediction(self, prediction_id, prediction, model_id, image_path):
+    def delete_prediction(self, prediction_id):
         try:
             with sqlite3.connect(self.database_path) as connection:
-                self.logger.info("function: {0} param: {1}, {2}, {3}, {4}".format("update_prediction", prediction_id, prediction, model_id, image_path))
-                query = """ UPDATE predictions SET 
-                prediction_status = ?, 
-                prediction = ?, 
-                image_path = ?, 
-                model_id = ?, 
-                prediction_time = ? 
-                WHERE prediction_id = ?;"""
-                cursor = connection.cursor()
-                # convert types
-                cursor.execute(query, (200, str(prediction), os.path.normpath(image_path), model_id, int(time.time()), prediction_id))
+                self.logger.info("function: {0} param: {1}".format("delete_prediction", prediction_id))
+                query = """DELETE FROM predictions WHERE prediction_id = ?;"""
+                cursor = connection.cursor()   
+                cursor.execute(query, (prediction_id,))
                 connection.commit()
         except:
             self.logger.critical("", exc_info=True)
 
 
-    def update_prediction_status(self, prediction_id, prediction_status):
+    def update_successful_prediction(self, prediction_id, prediction, model_info, model_id, image_path):
+        """updates successful prediction"""
         try:
             with sqlite3.connect(self.database_path) as connection:
-                self.logger.info("function: {0} param: {1}, {2}".format("update_prediction_status", prediction_id, prediction_status))
+                self.logger.info("function: {0} param: {1}, {2}, {3}, {4}, {5}".format("update_successful_prediction", prediction_id, prediction, model_info, model_id, image_path))
                 query = """ UPDATE predictions SET 
-                prediction_status = ? 
+                prediction_status = ?, 
+                prediction = ?, 
+                image_path = ?, 
+                model_info = ?, 
+                model_id = ? , 
+                prediction_time = ? 
+                WHERE prediction_id = ?;"""
+                cursor = connection.cursor()
+                # convert types
+                cursor.execute(query, (200, str(prediction), os.path.normpath(image_path), str(model_info), int(model_id), int(time.time()), prediction_id))
+                connection.commit()
+        except:
+            self.logger.critical("", exc_info=True)
+
+
+    def update_failed_prediction(self, prediction_id, prediction_status, model_info, model_id):
+        """updates failed prediction"""
+        try:
+            with sqlite3.connect(self.database_path) as connection:
+                self.logger.info("function: {0} param: {1}, {2}, {3}".format("update_failed_prediction", prediction_id, prediction_status, model_info, model_id))
+                query = """ UPDATE predictions SET 
+                prediction_status = ?,
+                model_info = ?,
+                model_id = ? 
                 WHERE prediction_id = ?;"""
                 cursor = connection.cursor()   
                 cursor.execute(query, (prediction_status, prediction_id))
@@ -85,8 +92,8 @@ class database_handler():
 
 
 
-
     def is_prediction_exists(self, prediction_id):
+        """returns true if prediction id exists on the database"""
         with sqlite3.connect(self.database_path) as connection:
             self.logger.info("function: {0} param: {1}".format("is_prediction_exists", prediction_id))
             
@@ -103,8 +110,9 @@ class database_handler():
 
 
     def get_prediction_json(self, prediction_id):
+        """returns prediction information as json by using prediction id"""
         with sqlite3.connect(self.database_path) as connection:
-            self.logger.info("function: {0} param: {1}".format("get_prediction", prediction_id))
+            self.logger.info("function: {0} param: {1}".format("get_prediction_json", prediction_id))
             
             query = "SELECT * FROM predictions WHERE prediction_id = ?;"
 
@@ -117,12 +125,13 @@ class database_handler():
 
                     # database can save single quotes but json function needs double quoutes so we have this sad line
                     predictions_json = json.loads(str(prediction[0][3]).replace("'","\""))
+                    model_info_json = json.loads(str( prediction[0][5]).replace("'","\""))
 
                     return {"prediction_id" : prediction[0][1], 
                             "prediction_status" : prediction[0][2], 
                             "predictions" : predictions_json["predictions"],
-                            "model_id" : prediction[0][5], 
-                            "prediction_time" : prediction[0][6]
+                            "model_info" : model_info_json,
+                            "prediction_time" : prediction[0][7]
                             }
                 else:
                     return  {"prediction_status" : prediction[0][2]}
