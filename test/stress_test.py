e11baf3b6c9951d30270f6a43d7ce6ae1d4ea776
can kurt <can.kurt.aa@gmail.com>
2020-10-22 20:09:55 +0300

    some testing changes

--- test/stress_test.py
+++ test/stress_test.py
@@ -43,11 +43,11 @@ if __name__ == "__main__":
     id_prefix = "TEST" 
     id_size = 12
     url = 'http://127.0.0.1:5000/upload'
-    test_image_folder = "test_images/food/asure"
-    model_names = ["food 10 vgg16", "food 10 densenet", "food 10 vgg16 2"]
+    test_image_folder = "test_images/images_from_darknet_repo"
+    model_names = ["darknet coco"]
     
-    workers = 30
-    request_count = 30
+    workers = 10
+    request_count = 10
 
     stress_gen = stress_generator(id_prefix=id_prefix, id_size=id_size, url=url, test_image_folder=test_image_folder, model_names=model_names)
     stress_gen.start_test(workers=workers, request_count=request_count)
