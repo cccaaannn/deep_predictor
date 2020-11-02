f0142c9ab7b9ca28f31be37395153e9667d005de
can kurt <can.kurt.aa@gmail.com>
2020-11-02 20:02:49 +0300

    waitress_server added for deploying with waitress

---
+++ waitress_server.py
@@ -0,0 +1,21 @@
+import sys
+sys.path.insert(0, "deep_predictor")
+from deep_predictor import create_app
+from waitress import serve
+
+# add logger file to waitress logger
+import logging
+logger = logging.getLogger("waitress")
+logger.setLevel(logging.WARN)
+file_handler = logging.FileHandler("deep_predictor/logs/waitress.log")
+logger.addHandler(file_handler)
+
+# serveing options
+app = create_app("deep_predictor/cfg/deep_predictor_small.cfg")
+host = "0.0.0.0"
+port = 5000
+threads = 5
+# url_scheme = "https"
+
+# start server
+serve(app, host=host, port=port, threads=threads)
