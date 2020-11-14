98d8f6cc2f39f31a0e2f7606e46b62f8861ef63f
can kurt <can.kurt.aa@gmail.com>
2020-11-14 16:25:42 +0300

    test endpoints added for testing during production, cfg files now has test options too

--- deep_predictor/flask_app.py
+++ deep_predictor/flask_app.py
@@ -30,36 +30,60 @@ def create_app(deep_predictor_cfg_path):
 
     # flask options
     secret_key = cfg["deep_predictor_options"]["flask_options"]["secret_key"]
+    max_content_length = eval(cfg["deep_predictor_options"]["flask_options"]["max_content_length"])
     debug = cfg["deep_predictor_options"]["flask_options"]["debug"]
 
     # upload options
-    max_content_length = eval(cfg["deep_predictor_options"]["upload_options"]["max_content_length"])
-    supported_extensions = cfg["deep_predictor_options"]["upload_options"]["supported_extensions"]
+    supported_extensions = cfg["deep_predictor_options"]["production"]["upload_options"]["supported_extensions"]
 
     # api options
-    prediction_id_length = cfg["deep_predictor_options"]["api_options"]["prediction_id_length"]
-    default_api_response = cfg["deep_predictor_options"]["api_options"]["default_api_response"]
-    get_prediction_endpoint = cfg["deep_predictor_options"]["api_options"]["get_prediction_endpoint"]
-    get_predictors_endpoint = cfg["deep_predictor_options"]["api_options"]["get_predictors_endpoint"]
+    prediction_id_length = cfg["deep_predictor_options"]["production"]["api_options"]["prediction_id_length"]
+    default_api_response = cfg["deep_predictor_options"]["production"]["api_options"]["default_api_response"]
+    get_prediction_endpoint = cfg["deep_predictor_options"]["production"]["api_options"]["get_prediction_endpoint"]
+    get_predictors_endpoint = cfg["deep_predictor_options"]["production"]["api_options"]["get_predictors_endpoint"]
 
     # path options
-    temp_save_path = cfg["deep_predictor_options"]["path_options"]["temp_save_path"]
-    database_path = cfg["deep_predictor_options"]["path_options"]["database_path"]
+    temp_save_path = cfg["deep_predictor_options"]["production"]["path_options"]["temp_save_path"]
+    database_path = cfg["deep_predictor_options"]["production"]["path_options"]["database_path"]
 
     # prediction options
-    default_predictor_name = cfg["deep_predictor_options"]["prediction_options"]["default_predictor_name"]
-
+    default_predictor_name = cfg["deep_predictor_options"]["production"]["prediction_options"]["default_predictor_name"]
 
     # create instace of db class
     db = database_handler(database_path = database_path, check_connection = True, create_table = True)
 
     # create predictors
     predictors = {}
-    for predictor in cfg["deep_predictor_options"]["prediction_options"]["predictors"]:
+    for predictor in cfg["deep_predictor_options"]["production"]["prediction_options"]["predictors"]:
         predictors.update({
-            predictor : deep_predictor(cfg["deep_predictor_options"]["prediction_options"]["predictors"][predictor])
+            predictor : deep_predictor(cfg["deep_predictor_options"]["production"]["prediction_options"]["predictors"][predictor])
         })
 
+
+    # --test options--
+
+    # upload options
+    TEST_supported_extensions = cfg["deep_predictor_options"]["test"]["upload_options"]["supported_extensions"]
+
+    # api options
+    TEST_prediction_id_length = cfg["deep_predictor_options"]["test"]["api_options"]["prediction_id_length"]
+    TEST_default_api_response = cfg["deep_predictor_options"]["test"]["api_options"]["default_api_response"]
+    TEST_get_prediction_endpoint = cfg["deep_predictor_options"]["test"]["api_options"]["get_prediction_endpoint"]
+    TEST_get_predictors_endpoint = cfg["deep_predictor_options"]["test"]["api_options"]["get_predictors_endpoint"]
+
+    # path options
+    TEST_temp_save_path = cfg["deep_predictor_options"]["test"]["path_options"]["temp_save_path"]
+    TEST_database_path = cfg["deep_predictor_options"]["test"]["path_options"]["database_path"]
+
+    # create instace of db class
+    TEST_db = database_handler(database_path = TEST_database_path, check_connection = True, create_table = True)
+    
+    # create predictors
+    TEST_predictors = {}
+    for predictor in cfg["deep_predictor_options"]["test"]["prediction_options"]["predictors"]:
+        TEST_predictors.update({
+            predictor : deep_predictor(cfg["deep_predictor_options"]["test"]["prediction_options"]["predictors"][predictor])
+        })
 
 
     # flask app
@@ -73,16 +97,14 @@ def create_app(deep_predictor_cfg_path):
         logger.debug("function: {0} method: {1}".format("home", request.method))
         return render_template("index.html"), 200
 
-
     @app.route('/result', methods=['GET'])
     def result():
         logger.debug("function: {0} method: {1}".format("result", request.method))
         return redirect(url_for("home")), 308
 
-
     @app.route('/upload', methods=['POST', 'GET'])
-    def upload_image():
-        logger.debug("function: {0} method: {1}".format("upload_files", request.method))
+    def upload():
+        logger.debug("function: {0} method: {1}".format("upload", request.method))
         if(request.method == 'POST'):
             
             # get elements form form
@@ -160,6 +182,7 @@ def create_app(deep_predictor_cfg_path):
         # get args
         arguments = request.args
         prediction_id = arguments.get(get_prediction_endpoint)
+        test_prediction_id = arguments.get(TEST_get_prediction_endpoint)
 
         if(prediction_id):
             prediction = db.get_prediction_json(prediction_id)
@@ -172,7 +195,97 @@ def create_app(deep_predictor_cfg_path):
             logger.warning("prediction_id is not provided")
             return default_api_response, 400
 
+
+    # -- test endpoints --
+    @app.route('/test-upload', methods=['POST', 'GET'])
+    def test_upload():
+        if(request.method == 'POST'):
+            logger.debug("function: {0} method: {1}".format("test_upload", request.method))
+            # get elements form form
+            uploaded_file = request.files['image']
+            filename = secure_filename(uploaded_file.filename)
+            model_name = request.form['model_name']
+            prediction_id = request.form['prediction_id']
+
+            # check form elements
+            if(filename == "" or prediction_id == "" or model_name == ""):
+                logger.warning("required form elements are empty")
+                abort(400, description="required form elements are empty")
+
+            if(len(prediction_id) != TEST_prediction_id_length):
+                logger.warning("prediction_id length is wrong")
+                abort(400, description="prediction_id length is wrong")
+
+            # check model name
+            if(model_name not in TEST_predictors):
+                logger.warning("model name is not exists")
+                abort(400, description="model name is not exists")
+
+            # check for collision
+            if(TEST_db.is_prediction_exists(prediction_id)):
+                logger.warning("prediction id exists")
+                abort(400, description="prediction id exists")
+
+            # check image extension
+            if(not image_operations.validate_extension(TEST_supported_extensions, filename, is_case_sensitive=False)):
+                logger.warning("image extension is not supported")
+                abort(400, description="image extension is not supported")
+
+            # generate unique id
+            # unique_full_filename = file_folder_operations.create_unique_file_name(os.path.join(temp_save_path, filename))
+            unique_filename = str(uuid.uuid4())
+            _, file_extension = os.path.splitext(filename)
+            unique_full_filename = os.path.join(TEST_temp_save_path, unique_filename + file_extension)
+            uploaded_file.save(unique_full_filename)
+
+            # check image resizability
+            status, unique_full_filename = image_operations.validate_image(unique_full_filename, try_to_convert = True, delete_unresizable = True)
+            if(not status):
+                logger.warning("image is not supported")
+                abort(400, description="image is not supported")
+
+            # prepare preediction
+            model_info, model_id = TEST_predictors[model_name].get_model_info()
+            TEST_db.create_prediction(prediction_id, model_info, model_id)
+
+            # start prediction on thread
+            pred_thread = prediction_thread(
+                TEST_database_path, 
+                unique_full_filename, 
+                prediction_id, 
+                predictor=TEST_predictors[model_name]
+                )
+            
+            pred_thread.start()
+
+            return "success", 201
+        else:
+            return redirect(url_for("home")), 308
+
+
+    @app.route('/test-api', methods=['GET'])
+    def test_api():
+        logger.debug("function: {0} method: {1}".format("test_api", request.method))
+
+        # get args
+        arguments = request.args
+        prediction_id = arguments.get(TEST_get_prediction_endpoint)
+
+        if(prediction_id):
+            prediction = TEST_db.get_prediction_json(prediction_id)
+            return prediction, 200
+
+        elif(TEST_get_predictors_endpoint in arguments):
+            return {TEST_get_predictors_endpoint : list(TEST_predictors.keys())}, 200
+
+        else:
+            logger.warning("prediction_id is not provided")
+            return TEST_default_api_response, 400
+
+
+    # return flask app for serving
     return app
+
 
 
 
