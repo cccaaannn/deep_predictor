15a70987da6ebbed1f818e49dbb6b96cd8e8768a
can kurt <can.kurt.aa@gmail.com>
2020-11-17 02:26:57 +0300

    recaptcha added

--- deep_predictor/static/js/upload_controller.js
+++ deep_predictor/static/js/upload_controller.js
@@ -1,12 +1,19 @@
-const form = document.getElementById("image-form");
+window.onload = function() {
+    // make capctcha required
+    var $recaptcha = document.querySelector('#g-recaptcha-response');
+    if($recaptcha) {
+        $recaptcha.setAttribute("required", "required");
+    }
 
-form.addEventListener("submit", post_form);
-
+    // listen submit form
+    const form = document.getElementById("image-form");
+    form.addEventListener("submit", post_form);
+};
 
 // generate prediction_id
 function generate_id(length) {
-    var result           = '';
-    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var result = '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     var charactersLength = characters.length;
     for ( var i = 0; i < length; i++ ) {
        result += characters.charAt(Math.floor(Math.random() * charactersLength));
@@ -14,12 +21,38 @@ function generate_id(length) {
     return result;
  }
 
-// generate the id when form is sent
+// on form post
 function post_form(e){
     const id = generate_id(32)
     document.getElementById("prediction_id").value = id;
-    console.log("prediction_id: " + id);
+    // console.log("prediction_id: " + id);
+    // document.getElementById("api_key").value = "";
+
+    // change elements when upload started
+    document.getElementById("file_chooser").style.display = "none";
+    document.getElementById("dropdown").style.display = "none";
+    document.getElementById("submit_button").style.display = "none";
+    // form.submit_button.disabled = true;
+    document.getElementById("recaptcha").style.display = "none";
+
+    // show Uploading with progress bar
+    document.getElementById('header_div').innerHTML = `
+        <h1>Uploading</h1>
+        </br>
+        <div id='bar' class='indeterminate-progress-box'>
+            <div class='indeterminate-progress-block'>
+            </div>
+        </div>
+    `
 }
+
+// --- dropdown functions --- //
+
+// on dom load
+$('document ').ready(function(){    
+    $('.dropdown-toggle').html($('.dropdown-menu a').html());
+    $('#model_name').val($('.dropdown-menu a').html());
+})
 
 // file input text change
 $(".custom-file-input").on("change", function() {
@@ -32,19 +65,3 @@ $('.dropdown-menu a').on('click', function(){
     $('.dropdown-toggle').html($(this).html());
     $('#model_name').val($(this).html());
 })
-
-// on dom load
-$('document ').ready(function(){    
-    $('.dropdown-toggle').html($('.dropdown-menu a').html());
-    $('#model_name').val($('.dropdown-menu a').html());
-})
-
-//   $('.dropdown').on('hidden.bs.dropdown', function () {
-    
-//     })
-
-//   $('.dropdown').click(function(){
-
-// $('.dropdown-menu').toggleClass('show');
-// console.log($(this).value);
-// });
