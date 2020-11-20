a2bdec1d7013ecd99fd32557934c2770d6eadfd8
can kurt <can.kurt.aa@gmail.com>
2020-11-21 02:25:17 +0300

    since flask app now is a class waitress_server updated to work with it

--- waitress_server.py
+++ waitress_server.py
@@ -1,6 +1,7 @@
 import sys
 sys.path.insert(0, "deep_predictor")
-from deep_predictor import create_app
+# from deep_predictor import create_app
+from deep_predictor import flask_app
 from waitress import serve
 
 # add logger file to waitress logger
@@ -11,7 +12,8 @@ file_handler = logging.FileHandler("deep_predictor/logs/waitress.log")
 logger.addHandler(file_handler)
 
 # serveing options
-app = create_app("deep_predictor/cfg/deep_predictor_small.cfg")
+deep_predictor = flask_app("deep_predictor/cfg/deep_predictor_small.cfg")
+app = deep_predictor.create_app()
 host = "0.0.0.0"
 port = 5000
 threads = 5
